315a4ab9ca0b13d30c825605fa60453564da3119
SinaBhr <125271549+SinaBhr@users.noreply.github.com>
2023-02-13 16:44:34 +0330

    Add files via upload

---
+++ جلسه 3.py
@@ -0,0 +1,16 @@
+num1="74.250"
+num2="13"
+
+num1=float(num1)
+num2=int(num2)
+num2=float(num2)
+
+num3=num1/num2
+num3=round(num3,2)
+print(num3)
+
+num3=int(num3)
+print(num3)
+
+num3=num1%num2
+print(num3)
